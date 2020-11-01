6a948201e5d97e515fe4ff7e94e2e1a9703780fe
Saud Iqbal <saudiqbal@users.noreply.github.com>
2020-11-01 01:41:13 -0400

    Update getIP.php

--- backend/getIP.php
+++ backend/getIP.php
@@ -14,52 +14,51 @@ if(isset($_GET["cors"])){
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=0');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['X-Real-IP'])) {
-    $ip = $_SERVER['X-Real-IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    $ip = preg_replace("/,.*/", "", $ip); # hosts are comma-separated, client is first
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
 
-$ip = preg_replace("/^::ffff:/", "", $ip);
+$ip = $_SERVER['REMOTE_ADDR'];
 
 if ($ip == "::1") { // ::1/128 is the only localhost ipv6 address. there are no others, no need to strpos this
     //echo json_encode(['processedString' => $ip . " - localhost IPv6 access", 'rawIspInfo' => ""]);
 	echo '{"processedString":"'.$ip.' - localhost IPv6 access","rawIspInfo":""}';
-    die();
+    exit;
 }
-if (stripos($ip, 'fe80:') === 0) { // simplified IPv6 link-local address (should match fe80::/10)
+elseif (stripos($ip, 'fe80:') === 0) { // simplified IPv6 link-local address (should match fe80::/10)
     //echo json_encode(['processedString' => $ip . " - link-local IPv6 access", 'rawIspInfo' => ""]);
 	echo '{"processedString":"'.$ip.' - link-local IPv6 access","rawIspInfo":""}';
-    die();
+    exit;
 }
-if (strpos($ip, '127.') === 0) { //anything within the 127/8 range is localhost ipv4, the ip must start with 127.0
+elseif (stripos($ip, 'fd') === 0) { // simplified IPv6 link-local address (should match fe80::/10)
+    //echo json_encode(['processedString' => $ip . " - link-local IPv6 access", 'rawIspInfo' => ""]);
+	echo '{"processedString":"'.$ip.' - link-local IPv6 access","rawIspInfo":""}';
+    exit;
+}
+elseif (strpos($ip, '127.') === 0) { //anything within the 127/8 range is localhost ipv4, the ip must start with 127.0
     //echo json_encode(['processedString' => $ip . " - localhost IPv4 access", 'rawIspInfo' => ""]);
 	echo '{"processedString":"'.$ip.' - localhost IPv4 access","rawIspInfo":""}';
-    die();
+    exit;
 }
-if (strpos($ip, '10.') === 0) { // 10/8 private IPv4
+elseif (strpos($ip, '10.') === 0) { // 10/8 private IPv4
     //echo json_encode(['processedString' => $ip . " - private IPv4 access", 'rawIspInfo' => ""]);
 	echo '{"processedString":"'.$ip.' - private IPv4 access","rawIspInfo":""}';
-    die();
+    exit;
 }
-if (preg_match('/^172\.(1[6-9]|2\d|3[01])\./', $ip) === 1) { // 172.16/12 private IPv4
+elseif (preg_match('/^172\.(1[6-9]|2\d|3[01])\./', $ip) === 1) { // 172.16/12 private IPv4
     //echo json_encode(['processedString' => $ip . " - private IPv4 access", 'rawIspInfo' => ""]);
 	echo '{"processedString":"'.$ip.' - private IPv4 access","rawIspInfo":""}';
-    die();
+    exit;
 }
-if (strpos($ip, '192.168.') === 0) { // 192.168/16 private IPv4
+elseif (strpos($ip, '192.168.') === 0) { // 192.168/16 private IPv4
     //echo json_encode(['processedString' => $ip . " - private IPv4 access", 'rawIspInfo' => ""]);
 	echo '{"processedString":"'.$ip.' - private IPv4 access","rawIspInfo":""}';
-    die();
+    exit;
 }
-if (strpos($ip, '169.254.') === 0) { // IPv4 link-local
+elseif (strpos($ip, '169.254.') === 0) { // IPv4 link-local
     //echo json_encode(['processedString' => $ip . " - link-local IPv4 access", 'rawIspInfo' => ""]);
 	echo '{"processedString":"'.$ip.' - link-local IPv4 access","rawIspInfo":""}';
-    die();
+    exit;
+}
+else { // IPv4 link-local
+	echo '{"processedString":"'.$ip.'","rawIspInfo":""}';
+    exit;
 }
 ?>
